12e1270594397b26ac537ae1ae6e13dd3d2b14f1
Tesfatsion <tesfatsion1st@gmail.com>
2024-11-05 06:24:56 -0800

    comments removed

--- src/ui/Textarea.jsx
+++
@@ -1,8 +0,0 @@
-const Textarea = (props) => (
-  <textarea
-    className="max- mx-3 h-32 w-full rounded-md border border-gray-300 bg-gray-50 p-2 shadow-sm"
-    {...props}
-  />
-);
-
-export default Textarea;
