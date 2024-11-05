b429ce0ed3813a405704f3d2880ca8c8105cedd2
Tesfatsion <tesfatsion1st@gmail.com>
2024-11-05 13:54:12 -0800

    portfolio style edit

--- src/features/portfolio/Portfolio.jsx
+++ src/features/portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio({ portfolio }) {
     navigate("/portfoliodetail", { state: { portfolio } });
   }
   return (
-    <div className="flex w-full flex-col items-center rounded-lg bg-white p-6 text-center shadow-lg md:w-1/4">
+    <div className="flex w-full flex-col items-center rounded-lg border-x-4 border-yellow-600 bg-white p-6 text-center shadow-lg md:w-1/4">
       {/* // <div className="flex w-full flex-col items-center rounded-lg bg-white p-6 text-center shadow-lg lg:w-1/4"> */}
       <div className="relative" key={portfolio.id}>
         <img
@@ -16,13 +16,10 @@ export default function Portfolio({ portfolio }) {
           className="h-40 max-w-full rounded-md object-cover"
           alt="Graphic Portfolio"
         />
-        <div className="absolute bottom-0 left-1/2 mb-2 flex -translate-x-1/2 transform gap-4 space-x-2 whitespace-nowrap">
-          <span className="rounded-lg bg-yellow-600 bg-opacity-100 px-4 py-2 font-mono font-bold text-white hover:bg-teal-600">
-            {portfolio.name}
-          </span>
+        <div className="relative bottom-0 mb-2 flex justify-end">
           <button
             onClick={handleClick}
-            className="rounded-lg bg-yellow-600 bg-opacity-100 px-4 py-2 font-mono font-bold text-white hover:bg-teal-600"
+            className="absolute bottom-0 right-0 mb-2 mr-2 rounded-lg bg-yellow-600 px-4 py-2 font-mono font-bold text-white hover:bg-teal-600"
           >
             More...
           </button>
